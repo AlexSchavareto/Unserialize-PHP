69c3c5cc6019f3609247c9fa26f8909c1c6815ac
Alex Schavareto <schavareto@yahoo.com.br>
2022-04-19 21:41:25 -0300

    Scripts para deserializar

--- unserialize2.php
+++ unserialize2.php
@@ -1,1 +1,22 @@
-ÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿÿ
+<?php
+
+
+if (!isset($argv[1]) || !is_file($argv[1])) {
+    echo 'Favor informar o Arquivo Serializado';
+    die;
+}
+
+if (!isset($argv[2])) {
+    echo 'Favor informar o Arquivo SaÃ­da';
+    die;
+}
+
+
+$a = file_get_contents($argv[1]);
+$arrayResultado = unserialize($a);
+$conteudoPrint = print_r($arrayResultado,true);
+
+
+file_put_contents($argv[2],$conteudoPrint);
+
+
